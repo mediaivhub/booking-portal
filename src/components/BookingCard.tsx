3f33e5d6a7a9ca1c7740b27bd04ba76fa19732d1
anvar010 <anvarshaknavas588@gmail.com>
2026-08-24 12:21:00 +0400

    Relabel card field to 'Order ID'

--- src/components/BookingCard.tsx
+++ src/components/BookingCard.tsx
@@ -104,7 +104,7 @@ export default function BookingCard({
               <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2" />
               <rect x="9" y="3" width="6" height="4" rx="1" />
             </svg>
-            <span>Order: {booking.orderId}</span>
+            <span>Order ID: {booking.orderId}</span>
           </div>
         )}
       </div>
